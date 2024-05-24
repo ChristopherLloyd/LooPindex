0c2b7d7b7fd87243996b0eff602343344fa2a0a6
ben <bwstucky@gmail.com>
2024-05-23 20:33:32 -0500

    Directory cleanup and adding first catalog

---
+++ catalogs/spheriloops/runToCompile.py
@@ -0,0 +1,14 @@
+from subprocess import call #used for running external scripts
+filename = "tex/pinSets"
+call(['pdflatex', '--shell-escape', '-halt-on-error', '-output-directory', filename.split("/")[0], filename+".tex"])
+#call twice to fix references
+call(['bibtex',filename])
+call(['pdflatex', '--shell-escape', '-halt-on-error', '-output-directory', filename.split("/")[0], filename+".tex"])
+call(['pdflatex', '--shell-escape', '-halt-on-error', '-output-directory', filename.split("/")[0], filename+".tex"]) 
+try:
+    #os.remove(filename+".aux")
+    #os.remove(filename+".log")
+    #os.remove(filename+".toc")
+    shutil.rmtree( "svg-inkscape/" )
+except FileNotFoundError:
+    pass
