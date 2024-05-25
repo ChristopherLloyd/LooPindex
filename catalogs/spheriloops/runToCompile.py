722a847ba2da388f82d4538ee226a82902ccaa7b
ben <bwstucky@gmail.com>
2024-05-25 08:25:21 -0500

    Minor tweaks

--- catalogs/spheriloops/runToCompile.py
+++ catalogs/spheriloops/runToCompile.py
@@ -1,15 +1,15 @@
 from subprocess import call #used for running external scripts
-import shutil
+#import shutil
 filename = "tex/spheriloops"
 call(['pdflatex', '--shell-escape', '-halt-on-error', '-output-directory', filename.split("/")[0], filename+".tex"])
 #call twice to fix references
 call(['bibtex',filename])
 call(['pdflatex', '--shell-escape', '-halt-on-error', '-output-directory', filename.split("/")[0], filename+".tex"])
 call(['pdflatex', '--shell-escape', '-halt-on-error', '-output-directory', filename.split("/")[0], filename+".tex"]) 
-try:
+#try:
     #os.remove(filename+".aux")
     #os.remove(filename+".log")
     #os.remove(filename+".toc")
-    shutil.rmtree( "svg-inkscape/" )
-except FileNotFoundError:
-    pass
+#    shutil.rmtree( "svg-inkscape/" )
+#except FileNotFoundError:
+#    pass
